33517cd61e3cb2b75348c9f12e9197fe65e6caa7
Tom Wiesing <tkw01536@gmail.com>
2019-03-08 21:10:36 +0100

    Update Travis Tests
    Previously, the Travis Tests ran both Node and Python tests for serveral
    versions of Python. This commit updates the test script to seperate
    python and node tests when run on Travis.

--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
+set -e
 
-node src/js/test.js && python src/py/test.py
+
+# If we are on travis, run only tests for specific languages
+if [ "$TRAVIS" == "true" ]; then
+    if [ ! -z "$TRAVIS_NODE_VERSION" ]; then
+        node src/js/test.js
+    fi
+    if [ ! -z "$TRAVIS_PYTHON_VERSION" ]; then
+        python src/py/test.py
+    fi
+# Else we are local and run both tests
+else
+    node src/js/test.js
+    python src/py/test.py
+fi
