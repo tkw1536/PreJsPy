7bd591825604a68e801a24cd7a93fe3c41c7b587
Tom Wiesing <tkw01536@gmail.com>
2020-06-06 20:52:19 +0200

    example.js: Remove dead code

--- example.js
+++ example.js
@@ -1,6 +1,5 @@
 var PreJsPy = require("./src/js/PreJsPy.js");
 
-
 /**
  * Parses a single expression and prints the result to stdout.
  * @param {string} expr Expression to parse.
@@ -9,20 +8,6 @@ function run_example(expr){
     var p = new PreJsPy.PreJsPy();
     console.log(p.parse(expr));
 }
-
-/**
- * Utility function to create a test case.
- *
- * @param inp {string} Input to use.
- * @param msg {string} Message for test case.
- */
-function make_testcase(inp, msg) {
-    var p = PreJsPy.PreJsPy();
-    var out = p.parse(inp);
-
-    console.log('{\n    "input": '+JSON.stringify(inp)+',\n    "output": '+JSON.stringify(out)+',\n    "message": '+JSON.stringify(msg)+'\n}');
-}
-
 
 function main(){
     run_example('"Hello world"');
